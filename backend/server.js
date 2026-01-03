1a0018d21eb0754d71a6bda2dcedd212ffb79b39
deepakumari867 <deepakumari2301@gmail.com>
2026-01-03 17:43:21 +0530

    All

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import clothRoutes from "./routes/clothRoutes.js";
-
+import aiRoutes from "./routes/aiRoutes.js"
 dotenv.config();
 
 const app = express();
@@ -13,11 +13,12 @@ app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("💚 MongoDB Atlas Connected"))
-  .catch(err => console.log("❌ Mongo Error:", err.message));
+  .then(() => console.log("MongoDB Atlas Connected"))
+  .catch(err => console.log("Mongo Error:", err.message));
 
-app.use("/api/clothes", clothRoutes);
+app.use("/api/clothesApi", clothRoutes);
+app.use("/api/aiApi",aiRoutes);
 
 app.listen(process.env.PORT, () => {
-  console.log(`🚀 Backend running on ${process.env.PORT}`);
+  console.log(` Backend running on ${process.env.PORT}`);
 });
