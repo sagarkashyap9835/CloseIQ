ac8437a80bcf73555435c6e8521b7a85c0b07b1d
sagarkashyap9835 <vidyasagar983531@gmail.com>
2026-01-12 11:18:44 +0530

    dfgh

--- my-project/src/components/api/aiApi.js
+++ my-project/src/components/api/aiApi.js
@@ -1,6 +1,15 @@
+// import axios from "axios";
+
+// export const detectColorsAndSuggest = async (image) => {
+//   const res = await axios.post("http://localhost:5000/api/ai/detect", { image });
+//   return res.data;
+// };
+
+
 import axios from "axios";
 
 export const detectColorsAndSuggest = async (image) => {
-  const res = await axios.post("http://localhost:5000/api/ai/detect", { image });
+  // Yahan "aiApi" likhein kyunki backend mein yahi define hai
+  const res = await axios.post("http://localhost:5000/api/aiApi/detect", { image });
   return res.data;
 };
